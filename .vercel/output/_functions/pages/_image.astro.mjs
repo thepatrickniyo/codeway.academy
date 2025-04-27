452e65ee936a6fe0d86ea1e5f89f430adb9a4731
Patrick Niyo <patrickniyogitare28@gmail.com>
2025-04-27 18:13:29 +0200

    ingore temp dirs

--- .vercel/output/_functions/pages/_image.astro.mjs
+++
@@ -1,2 +0,0 @@
-export { a as page } from '../chunks/generic_Yac2gSzR.mjs';
-export { renderers } from '../renderers.mjs';
--- .vercel/output/static/_astro/studio-component.DqlOWcqx.js
+++
@@ -1,1 +0,0 @@
-import{au as p}from"./studio-component.-Xm003DW.js";import"./client.32G2rpbp.js";export{p as StudioComponent};
